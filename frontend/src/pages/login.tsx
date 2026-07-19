0bdae01361f2c40e8fb342476b17442b9b8187de
Zach Ruffin <faintdeception@gmail.com>
2026-07-19 08:14:38 -0400

    chore: fix linting error
    forgot to delete `handleRequestAccess` method from login.tsx.

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -68,12 +68,6 @@ export default function Login() {
     }
   };
 
-  const handleRequestAccess = () => {
-    if (requestAccessUrl) {
-      window.location.href = requestAccessUrl;
-    }
-  };
-
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
